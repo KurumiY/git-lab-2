67d1b02b04518c6cc95fcf4e770323dba49f3c73
Kurumi Yamada <ky141817@ohio.edu>
2021-01-26 20:04:43 -0500

    added file

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Kurumi Yamada>
+ *        Date: <01/26/2021>
+ * Description: lab2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
